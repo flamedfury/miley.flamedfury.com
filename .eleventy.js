8e4937efdc50496771daf2e9423c3bab3ae8254a
flamedfury <shadybrothers@gmail.com>
2022-04-04 18:06:05 +1200

    add photo gallery

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,26 @@
+// Import fast-glob package
+const fg = require('fast-glob');
+
+// Run search for images in /gallery
+const galleryImages = fg.sync(['**/gallery/*', 'img']);
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/css");
 	eleventyConfig.addPassthroughCopy("src/img");
 	eleventyConfig.addPassthroughCopy("src/js");
+	eleventyConfig.addPassthroughCopy("src/gallery");
 
 	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+	//Create collection of gallery images
+	eleventyConfig.addCollection('gallery', function (collection) {
+		return galleryImages;
+	});
 
 	return {
 		dir: {
 			// enable passthrough file copy for static assets.
 			passthroughFileCopy: true,
-
-			// set nunjucks as our template engine
-			// htmlTemplateEngine: 'njk',
-			// markdownTemplateEngine: 'njk',
-			// dataTemplateEngine: 'njk',
-			// templateFormats: ["html", "njk", "md"],
 
 			// set our prefered input and output directories
 			input: "src",
